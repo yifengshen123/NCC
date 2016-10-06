c8e7ce7d0a31565080178fc1ca5fabedf9d456ff
seko99 <seko99@mail.ru>
2016-10-06 07:01:42 +0300

    NetworkDevices (in progress)

--- src/main/java/com/NccNetworkDevices/NccNetworkDeviceType.java
+++ src/main/java/com/NccNetworkDevices/NccNetworkDeviceType.java
@@ -1,9 +1,31 @@
 package com.NccNetworkDevices;
 
+import com.NccSystem.NccAbstractData;
+
+import java.sql.SQLException;
+
 /**
  * Created by root on 05.10.16.
  */
-public class NccNetworkDeviceType {
+public class NccNetworkDeviceType extends NccAbstractData<NccNetworkDeviceType> {
     public Integer id;
     public String typeName;
+    public Integer mibSet;
+
+    @Override
+    public NccNetworkDeviceType fillData(){
+        NccNetworkDeviceType type = new NccNetworkDeviceType();
+
+        try {
+            type.id = rs.getInt("id");
+            type.typeName = rs.getString("typeName");
+            type.mibSet = rs.getInt("mibSet");
+
+            return type;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
